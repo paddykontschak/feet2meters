5037518f4b370815b9668f5569f0f1b1178db72a
Patrick Kontschak <patrick.kontschak@gmail.com>
2012-08-06 12:33:09 -0400

    Create UI elements
    I added an input field where the user can enter a number, a button that, when triggered, will calculate the given number (interpreted as feet) to meters and a label that will output the calculated number.
    
    At this point the button doesn't do anything though. I still have to define the function triggered by the button.
    
    Signed-off-by: Patrick Kontschak <patrick.kontschak@gmail.com>

--- feet2meters.rb
+++ feet2meters.rb
@@ -4,3 +4,8 @@ require 'tkextlib/tile' # load themed widgets
 root = TkRoot.new {title "Feet to Meters"} # create main window with title "Feet to Meters"
 content = Tk::Tile::Frame.new(root) {padding "3 3 12 12"}.grid(:sticky => 'nsew') # create frame widget. All UI content will be here.
 TkGrid.columnconfigure root, 0, :weight => 1; TkGrid.rowconfigure root, 0, :weight => 1 # if window size changes (resize) tell frame to adjust
+
+$feet = TkVariable.new; $meters = TkVariable.new # variables for feet and meters
+f = Tk::Tile::Entry.new(content) {width 7; textvariable $feet}.grid( :column => 2, :row => 1, :sticky => 'we' ) # create entry widget .Type number of feet
+Tk::Tile::Label.new(content) {textvariable $meters}.grid( :column => 2, :row => 2, :sticky => 'we'); # create label. Will output number of meters
+Tk::Tile::Button.new(content) {text 'Calculate'; command {calculate}}.grid( :column => 3, :row => 3, :sticky => 'w') # create button. Click to calculate feet to meters
